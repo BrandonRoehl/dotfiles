6e4d27f477b8ea4696ac139c4501167f3ef8385c
Brandon Roehl <brandon.roehl@jamf.com>
2024-10-22 16:48:06 -0500

    add copilot

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -980,6 +980,13 @@ require("lazy").setup({
 		event = "VimEnter",
 	},
 
+	{ -- Easymotion you know why you are here
+		"github/copilot.vim",
+		-- version = "1.41.x",
+		-- lazy = false,
+		event = "VimEnter",
+	},
+
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
