2c8ec3e31d7b91a61a0b30a7d5884094610b4ccb
Brandon Roehl <brandon.roehl@jamf.com>
2024-11-12 12:38:23 -0600

    theme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -11,7 +11,7 @@ return {
 			style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
 			light_style = "day", -- The theme is used when the background is set to light
 			transparent = true, -- Enable this to disable setting the background color
-			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+			terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
 		},
 		init = function()
 			-- Load the colorscheme here.
