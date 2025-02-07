5e070855f068b60f6797f58ac1aa21f3a81a3333
Brandon Roehl <brandon.roehl@jamf.com>
2025-02-07 08:56:52 -0600

    add chat

--- config/nvim/lua/plugins/autocomplete.lua
+++ config/nvim/lua/plugins/autocomplete.lua
@@ -1,28 +1,6 @@
 -- Autocompletion
 return {
 	{
-		"github/copilot.vim",
-		cmd = "Copilot",
-		build = ":Copilot auth",
-		event = "BufWinEnter",
-		-- event = "VeryLazy",
-		init = function()
-			vim.g.copilot_no_maps = true
-		end,
-		config = function()
-			-- Block the normal Copilot suggestions
-			vim.api.nvim_create_augroup("github_copilot", { clear = true })
-			for _, event in pairs({ "FileType", "BufUnload", "BufEnter" }) do
-				vim.api.nvim_create_autocmd({ event }, {
-					group = "github_copilot",
-					callback = function()
-						vim.fn["copilot#On" .. event]()
-					end,
-				})
-			end
-		end,
-	},
-	{
 		"saghen/blink.cmp",
 		event = "InsertEnter",
 		-- optional: provides snippets for the snippet source
