b57ad2a023f187abaaa242961b06133b5efb708a
Brandon Roehl <me@brandonroehl.org>
2025-02-26 18:08:31 -0600

    dashboard

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -70,6 +70,80 @@ return {
 			--  Check out: https://github.com/echasnovski/mini.nvim
 		end,
 	},
+	--- @type LazyPluginSpec
+	{
+		"nvimdev/dashboard-nvim",
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+		-- event = "VimEnter",
+		lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+		opts = function()
+			-- 			local logo = [[
+			-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+			-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+			-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+			-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+			-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+			-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+			-- ]]
+			-- 			logo = string.rep("\n", 8) .. logo .. "\n\n"
+			-- 			header = vim.split(logo, "\n"),
+			local opts = {
+				theme = "doom",
+				hide = {
+					-- this is taken care of by lualine
+					-- enabling this messes up the actual laststatus setting after loading a file
+					statusline = false,
+				},
+				config = {
+					week_header = {
+						enable = true,
+						-- concat = "concat",
+						-- append = { "append" },
+					},
+					-- header = vim.split(logo, "\n"),
+					-- stylua: ignore
+					center = {
+						{ action = 'Telescope find_files cwd=',               desc = " Find File",    icon = " ", key = "f" },
+						{ action = "ene | startinsert",                       desc = " New File",     icon = " ", key = "n" },
+						{ action = 'Telescope oldfiles cwd=',                 desc = " Recent Files", icon = " ", key = "r" },
+						{ action = 'Telescope live_grep cwd=',                desc = " Find Text",    icon = " ", key = "g" },
+						{ action = 'Telescope find_files cwd=~/.config/nvim', desc = " Config",       icon = " ", key = "c" },
+						{ action = "LazyGit",                                 desc = " Lazy Git",     icon = " ", key = "x" },
+						{ action = "Lazy",                                    desc = " Lazy",         icon = "󰒲 ", key = "l" },
+						{ action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",  icon = " ", key = "q" },
+						-- { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+					},
+					footer = function()
+						local stats = require("lazy").stats()
+						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+						return {
+							"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
+						}
+					end,
+				},
+			}
+
+			for _, button in ipairs(opts.config.center) do
+				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+				button.key_format = "  %s"
+			end
+
+			-- open dashboard after closing lazy
+			if vim.o.filetype == "lazy" then
+				vim.api.nvim_create_autocmd("WinClosed", {
+					pattern = tostring(vim.api.nvim_get_current_win()),
+					once = true,
+					callback = function()
+						vim.schedule(function()
+							vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+						end)
+					end,
+				})
+			end
+
+			return opts
+		end,
+	},
 }
 -- Better Around/Inside textobjects
 --
