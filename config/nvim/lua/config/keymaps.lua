e8f38540880b5e7515e5c20f9b41d38a5e5064e0
Brandon Roehl <brandon.roehl@jamf.com>
2025-03-11 10:45:05 -0500

    another easy access

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -30,6 +30,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-d>", "<C-w><C-d>", { desc = "Show the diagnostics under the cursor" })
 
 -- tab stuff
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnew +Dashboard<CR>", { desc = "[T]ab [N]ew" })
