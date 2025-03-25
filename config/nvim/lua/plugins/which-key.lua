361f22ad15cd67ca9a8399e0f056e83fffd40960
Brandon Roehl <brandon.roehl@jamf.com>
2025-03-25 11:27:54 -0500

    options for none

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -30,7 +30,7 @@ return {
 		---@type wk.Opts
 		opts = {
 			---@type false | "classic" | "modern" | "helix"
-			preset = "helix",
+			preset = vim.g.border == "none" and "classic" or "helix",
 			icons = {
 				-- set icon mappings to true if you have a Nerd Font
 				mappings = vim.g.have_nerd_font,
