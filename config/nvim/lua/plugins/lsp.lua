37af762131fff65439ae899d57fad22db4f1b562
Brandon Roehl <brandon.roehl@jamf.com>
2025-03-25 09:27:31 -0500

    fix autocommands

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -103,36 +103,38 @@ return {
 					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-					-- The following two autocommands are used to highlight references of the
+					-- The following autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup =
-							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						-- vim.cmd([[
+						-- hi! LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+						-- hi! LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+						-- hi! LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+						-- ]])
+						vim.api.nvim_create_augroup("lsp_document_highlight", {
+							clear = false,
+						})
+						vim.api.nvim_clear_autocmds({
 							buffer = event.buf,
-							group = highlight_augroup,
+							group = "lsp_document_highlight",
+						})
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							group = "lsp_document_highlight",
+							buffer = event.buf,
 							callback = vim.lsp.buf.document_highlight,
 						})
-
 						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							group = "lsp_document_highlight",
 							buffer = event.buf,
-							group = highlight_augroup,
 							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-							end,
 						})
 					end
 
+					-- The following auto commands trigger the diagnostics for what you are hovering
 					vim.api.nvim_create_autocmd("CursorHold", {
 						buffer = event.buf,
 						callback = function()
