3f11cc9aaf59c96753e7e8ff02f4323312f3174b
Brandon Roehl <brandon.roehl@jamf.com>
2025-04-04 08:39:09 -0500

    keybinds

--- config/nvim/lua/plugins/easymotion.lua
+++ config/nvim/lua/plugins/easymotion.lua
@@ -9,13 +9,30 @@ return {
 		},
 		-- setting the keybinding for when to load it
 		keys = {
+			-- The easy motion binds
+			{ "<leader><leader>s", "<cmd>HopChar1<cr>", desc = "Search character" },
+			{ "<leader><leader>f", "<cmd>HopChar1AC<cr>", desc = "Find character forwards" },
+			{ "<leader><leader>F", "<cmd>HopChar1BC<cr>", desc = "Find character backwards" },
+			{ "<leader><leader>t", "<cmd>HopChar1CurrentLineAC<cr>", desc = "Til character forwards" },
+			{ "<leader><leader>T", "<cmd>HopChar1CurrentLineBC<cr>", desc = "Til character backwards" },
 			{ "<leader><leader>w", "<cmd>HopWordAC<cr>", desc = "Hop next word" },
 			{ "<leader><leader>b", "<cmd>HopWordBC<cr>", desc = "Hop previous word" },
-			{ "<leader><leader>s", "<cmd>HopChar1AC<cr>", desc = "Hop next search" },
-			{ "<leader><leader>S", "<cmd>HopChar1BC<cr>", desc = "Hop prev search" },
-			{ "<leader><leader>j", "<cmd>HopVerticalAC<cr>", desc = "Hop next line" },
-			{ "<leader><leader>k", "<cmd>HopVerticalBC<cr>", desc = "Hop prev line" },
-			{ "<leader><leader>^", "<cmd>HopLineStart<cr>", desc = "Hop line first non-blank" },
+			-- <leader><leader> l	Matches beginning & ending of word, camelCase, after _, and after # forwards
+			{ "<leader><leader>l", "<cmd>HopAnywhereAC<cr>", desc = "Hop forward" },
+			-- <leader><leader> h	Matches beginning & ending of word, camelCase, after _, and after # backwards
+			{ "<leader><leader>h", "<cmd>HopAnywhereBC<cr>", desc = "Hop backwards" },
+			-- <leader><leader> e	End of word forwards
+			-- <leader><leader> ge	End of word backwards
+			{ "<leader><leader>j", "<cmd>HopLineStartAC<cr>", desc = "Start of line forwards" },
+			{ "<leader><leader>k", "<cmd>HopLineStartBC<cr>", desc = "Start of line backwards" },
+			{ "<leader><leader>/", "<cmd>HopPattern<cr>", desc = "Search n-character" },
+			-- <leader><leader><leader> bdt	Til character
+			-- <leader><leader><leader> bdw	Start of word
+			-- <leader><leader><leader> bde	End of word
+			-- <leader><leader><leader> bdjk	Start of line
+			{ "<leader><leader><leader>j", "<cmd>HopAnywhere<cr>", desc = "JumpToAnywhere motion" },
+
+			-- Custom stuff I have added
 			{ "<leader><leader>0", "<cmd>HopLine<cr>", desc = "Hop begining of line" },
 		},
 	},
