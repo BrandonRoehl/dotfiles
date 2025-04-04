9d6d37b8c24c33d29352c206dfb4842b520ccae4
Brandon Roehl <brandon.roehl@jamf.com>
2025-04-04 09:36:46 -0500

    remove the event

--- config/nvim/lua/plugins/easymotion.lua
+++ config/nvim/lua/plugins/easymotion.lua
@@ -4,7 +4,6 @@ return {
 		-- piece of code was changed in 2020 so for now I will use hop.nvim
 		-- and now a fork of hop since the original is not being maintained
 		"smoka7/hop.nvim",
-		event = "BufWinEnter",
 		lazy = true,
 		opts = {
 			keys = "etovxqpdygfblzhckisuran",
