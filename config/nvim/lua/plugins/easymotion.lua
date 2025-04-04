bf5e1104383b2791088531b22b961b14be83b85b
Brandon Roehl <brandon.roehl@jamf.com>
2025-04-04 08:12:52 -0500

    add all easymotion commands

--- config/nvim/lua/plugins/easymotion.lua
+++ config/nvim/lua/plugins/easymotion.lua
@@ -1,5 +1,6 @@
 return {
 	{
+		-- lazy motion hasn't been updated in 7 years as of the time of running
 		"smoka7/hop.nvim",
 		event = "BufWinEnter",
 		lazy = true,
@@ -12,8 +13,10 @@ return {
 			{ "<leader><leader>b", "<cmd>HopWordBC<cr>", desc = "Hop previous word" },
 			{ "<leader><leader>s", "<cmd>HopChar1AC<cr>", desc = "Hop next search" },
 			{ "<leader><leader>S", "<cmd>HopChar1BC<cr>", desc = "Hop prev search" },
-			{ "<leader><leader>j", "<cmd>HopLineAC<cr>", desc = "Hop next line" },
-			{ "<leader><leader>k", "<cmd>HopLineBC<cr>", desc = "Hop prev line" },
+			{ "<leader><leader>j", "<cmd>HopVerticalAC<cr>", desc = "Hop next line" },
+			{ "<leader><leader>k", "<cmd>HopVerticalBC<cr>", desc = "Hop prev line" },
+			{ "<leader><leader>^", "<cmd>HopLineStart<cr>", desc = "Hop line first non-blank" },
+			{ "<leader><leader>0", "<cmd>HopLine<cr>", desc = "Hop begining of line" },
 		},
 	},
 }
