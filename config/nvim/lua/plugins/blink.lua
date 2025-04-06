236fcabb29fe6d2faf38e75e6363d57ecc6c5060
Brandon Roehl <me@brandonroehl.org>
2025-04-06 18:14:04 -0500

    format

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -4,25 +4,6 @@
 return {
 	--- @type LazyPluginSpec
 	{
-		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-		-- used for completion, annotations and signatures of Neovim apis
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
-				"nvim-dap-ui",
-			},
-		},
-	},
-	--- @type LazyPluginSpec
-	{
-		"Bilal2453/luvit-meta",
-		lazy = true,
-	},
-	--- @type LazyPluginSpec
-	{
 		"saghen/blink.cmp",
 		event = "InsertEnter",
 		-- optional: provides snippets for the snippet source
@@ -221,5 +202,24 @@ return {
 			},
 		},
 		opts_extend = { "sources.default" },
+	},
+	--- @type LazyPluginSpec
+	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				"nvim-dap-ui",
+			},
+		},
+	},
+	--- @type LazyPluginSpec
+	{
+		"Bilal2453/luvit-meta",
+		lazy = true,
 	},
 }
