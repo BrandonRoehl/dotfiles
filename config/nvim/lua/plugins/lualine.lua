baf180e0599f8c50b3f0f74550d666b240c687f6
Brandon Roehl <brandon.roehl@jamf.com>
2025-04-25 11:31:32 -0500

    lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -23,36 +23,115 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		version = "*",
-		opts = {
-			options = {
-				icons_enabled = vim.g.have_nerd_font,
-				-- theme = "tokyonight",
-				globalstatus = vim.o.laststatus == 3,
-				-- component_separators = vim.g.have_nerd_font and { left = "", right = "" }
-				-- 	or { left = "│", right = "│" },
-				-- section_separators = vim.g.have_nerd_font and { left = "", right = "" }
-				-- 	or { left = "", right = "" },
-				component_separators = { left = "│", right = "│" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
-					winbar = {},
+		event = "VeryLazy",
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = function()
+			-- PERF: we don't need this lualine require madness 🤷
+			local lualine_require = require("lualine_require")
+			lualine_require.require = require
+
+			-- local icons = LazyVim.config.icons
+
+			vim.o.laststatus = vim.g.lualine_laststatus
+
+			local opts = {
+				options = {
+					theme = "auto",
+					icons_enabled = vim.g.have_nerd_font,
+					globalstatus = vim.o.laststatus == 3,
+					-- component_separators = vim.g.have_nerd_font and { left = "", right = "" }
+					-- 	or { left = "│", right = "│" },
+					-- section_separators = vim.g.have_nerd_font and { left = "", right = "" }
+					-- 	or { left = "", right = "" },
+					component_separators = { left = "│", right = "│" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
+						winbar = {},
+					},
 				},
-			},
-			sections = {
-				-- Add CodeCompanion to the right side of the status line
-				lualine_x = {
-					-- This will be replaced by the actual component in the config function
-					-- this is registered in `lua/lualine/components`
-					"codecompanion",
-					"encoding",
-					"fileformat",
-					"filetype",
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = {
+						"diagnostics",
+						"filetype",
+						"filename",
+					},
+					lualine_x = {
+						Snacks.profiler.status(),
+						-- stylua: ignore
+						-- {
+						-- 	function() return require("noice").api.status.command.get() end,
+						-- 	cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+						-- 	color = function() return { fg = Snacks.util.color("Statement") } end,
+						-- },
+						-- -- stylua: ignore
+						-- {
+						-- 	function() return require("noice").api.status.mode.get() end,
+						-- 	cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+						-- 	color = function() return { fg = Snacks.util.color("Constant") } end,
+						-- },
+						-- stylua: ignore
+						{
+							function() return "  " .. require("dap").status() end,
+							cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+							color = function() return { fg = Snacks.util.color("Debug") } end,
+						},
+						-- stylua: ignore
+						{
+							require("lazy.status").updates,
+							cond = require("lazy.status").has_updates,
+							color = function() return { fg = Snacks.util.color("Special") } end,
+						},
+						{
+							"diff",
+							-- symbols = {
+							-- 	added = icons.git.added,
+							-- 	modified = icons.git.modified,
+							-- 	removed = icons.git.removed,
+							-- },
+							source = function()
+								local gitsigns = vim.b.gitsigns_status_dict
+								if gitsigns then
+									return {
+										added = gitsigns.added,
+										modified = gitsigns.changed,
+										removed = gitsigns.removed,
+									}
+								end
+							end,
+						},
+						-- This will be replaced by the actual component in the config function
+						-- this is registered in `lua/lualine/components`
+						"codecompanion",
+					},
+					lualine_y = {
+						"encoding",
+						"fileformat",
+					},
+					lualine_z = {
+						{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+						{ "location", padding = { left = 0, right = 1 } },
+						-- function()
+						-- 	return " " .. os.date("%R")
+						-- end,
+					},
 				},
-			},
-			extensions = { "lazy", "mason", "neo-tree", "nvim-dap-ui" },
-		},
+				extensions = { "lazy", "mason", "fzf", "neo-tree", "nvim-dap-ui" },
+			}
+
+			return opts
+		end,
 	},
 }
 -- Better Around/Inside textobjects
