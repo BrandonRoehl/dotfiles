807e45c6e10335a6aa9a968546d38689e267971a
Brandon Roehl <brandon.roehl@jamf.com>
2025-04-25 15:41:57 -0500

    move

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,24 +1,6 @@
 --- @module 'lazy'
 --- @return LazyPluginSpec[]
 return {
-	--- @return LazyPluginSpec
-	{ -- Highlight todo, notes, etc in comments
-		"folke/todo-comments.nvim",
-		event = "VeryLazy",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			signs = vim.g.have_nerd_font,
-			keywords = {
-				FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
-				TODO = { icon = " ", color = "info" },
-				HACK = { icon = " ", color = "warning" },
-				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-				NOTE = { icon = " ", color = "hint", alt = { "INFO", "MARK" } },
-				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-			},
-		},
-	},
 	--- @type LazyPluginSpec
 	{
 		"nvim-lualine/lualine.nvim",
--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -12,6 +12,23 @@ return {
 			require("plugins.codecompanion.fidget-spinner"):init()
 		end,
 	},
+	{
+		"folke/todo-comments.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			signs = vim.g.have_nerd_font,
+			keywords = {
+				FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO", "MARK" } },
+				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+		},
+	},
 	-- Detect tabstop and shiftwidth automatically
 	"tpope/vim-sleuth",
 }
