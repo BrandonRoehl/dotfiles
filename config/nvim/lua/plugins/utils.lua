eda08d93142990fc8f71588aef696661450f5842
Brandon Roehl <brandon.roehl@jamf.com>
2025-04-29 11:55:15 -0500

    move out of plugins

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -8,7 +8,7 @@ return {
 		opts = {},
 		config = function(_, opts)
 			require("fidget").setup(opts)
-			require("plugins.codecompanion.fidget-spinner"):init()
+			require("codecompanion.fidget-spinner"):init()
 		end,
 	},
 	{
