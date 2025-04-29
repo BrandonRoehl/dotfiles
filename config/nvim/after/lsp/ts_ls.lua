a3ef63cedabf20288e84f1f5173cd658449a3731
Brandon Roehl <brandon.roehl@jamf.com>
2025-04-29 08:25:28 -0500

    ts vue nonsence

--- config/nvim/after/lsp/ts_ls.lua
+++ config/nvim/after/lsp/ts_ls.lua
@@ -1,3 +1,9 @@
+local vue_plugin = {
+	name = "@vue/typescript-plugin",
+	location = "/usr/local/lib/node_modules/@vue/language-server",
+	languages = { "vue" },
+}
+
 return {
 	--- @type lsp.ClientCapabilities
 	capabilities = {
@@ -6,18 +12,22 @@ return {
 		},
 	},
 	init_options = {
-		plugins = { -- I think this was my breakthrough that made it work
-			{
-				name = "@vue/typescript-plugin",
-				location = "/usr/local/lib/node_modules/@vue/language-server",
-				languages = { "vue" },
-			},
-		},
+		plugins = nil,
 	},
 	before_init = function(_, config)
+		-- Check that we haven't set anything yet
+		if not config.init_options or config.init_options.plugins then
+			return
+		end
+
 		local lib_path = vim.fs.find("node_modules/@vue/language-server", { path = config.root_dir, upward = true })[1]
 		if lib_path then
-			config.init_options.plugins[0].location = lib_path
+			vue_plugin.location = lib_path
+		end
+		if vim.fs.exists(lib_path) then
+			config.init_options.plugins = { vue_plugin }
+		else
+			config.init_options.plugins = {}
 		end
 	end,
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
