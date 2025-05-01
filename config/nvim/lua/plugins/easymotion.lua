23d8a41aa29eeef2f3883a6aa850aa7c7bdadcda
Brandon Roehl <brandon.roehl@jamf.com>
2025-05-01 12:16:18 -0500

    add more modes

--- config/nvim/lua/plugins/easymotion.lua
+++ config/nvim/lua/plugins/easymotion.lua
@@ -21,30 +21,40 @@ return {
 		-- Setting the keybinding for when to load it
 		keys = {
 			-- The easy motion binds
-			{ "<leader><leader>s", "<cmd>HopChar1<cr>", desc = "Search character" },
-			{ "<leader><leader>f", "<cmd>HopChar1AC<cr>", desc = "Find character forwards" },
-			{ "<leader><leader>F", "<cmd>HopChar1BC<cr>", desc = "Find character backwards" },
-			{ "<leader><leader>t", "<cmd>HopChar1CurrentLineAC<cr>", desc = "Til character forwards" },
-			{ "<leader><leader>T", "<cmd>HopChar1CurrentLineBC<cr>", desc = "Til character backwards" },
-			{ "<leader><leader>w", "<cmd>HopWordAC<cr>", desc = "Start of word forwards" },
-			{ "<leader><leader>b", "<cmd>HopWordBC<cr>", desc = "Start of word backwards" },
+			{ "<leader><leader>s", "<cmd>HopChar1<cr>", desc = "Search character", mode = { "n", "v" } },
+			{ "<leader><leader>f", "<cmd>HopChar1AC<cr>", desc = "Find character forwards", mode = { "n", "v" } },
+			{ "<leader><leader>F", "<cmd>HopChar1BC<cr>", desc = "Find character backwards", mode = { "n", "v" } },
+			{
+				"<leader><leader>t",
+				"<cmd>HopChar1CurrentLineAC<cr>",
+				desc = "Til character forwards",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader><leader>T",
+				"<cmd>HopChar1CurrentLineBC<cr>",
+				desc = "Til character backwards",
+				mode = { "n", "v" },
+			},
+			{ "<leader><leader>w", "<cmd>HopWordAC<cr>", desc = "Start of word forwards", mode = { "n", "v" } },
+			{ "<leader><leader>b", "<cmd>HopWordBC<cr>", desc = "Start of word backwards", mode = { "n", "v" } },
 			-- <leader><leader> l Matches beginning & ending of word, camelCase, after _, and after # forwards
-			{ "<leader><leader>l", "<cmd>HopAnywhereAC<cr>", desc = "Hop forward" },
+			{ "<leader><leader>l", "<cmd>HopAnywhereAC<cr>", desc = "Hop forward", mode = { "n", "v" } },
 			-- <leader><leader> h Matches beginning & ending of word, camelCase, after _, and after # backwards
-			{ "<leader><leader>h", "<cmd>HopAnywhereBC<cr>", desc = "Hop backwards" },
+			{ "<leader><leader>h", "<cmd>HopAnywhereBC<cr>", desc = "Hop backwards", mode = { "n", "v" } },
 			-- <leader><leader> e End of word forwards
 			-- <leader><leader> ge End of word backwards
-			{ "<leader><leader>j", "<cmd>HopLineStartAC<cr>", desc = "Start of line forwards" },
-			{ "<leader><leader>k", "<cmd>HopLineStartBC<cr>", desc = "Start of line backwards" },
-			{ "<leader><leader>/", "<cmd>HopPattern<cr>", desc = "Search n-character" },
+			{ "<leader><leader>j", "<cmd>HopLineStartAC<cr>", desc = "Start of line forwards", mode = { "n", "v" } },
+			{ "<leader><leader>k", "<cmd>HopLineStartBC<cr>", desc = "Start of line backwards", mode = { "n", "v" } },
+			{ "<leader><leader>/", "<cmd>HopPattern<cr>", desc = "Search n-character", mode = { "n", "v" } },
 			-- <leader><leader><leader> bdt Til character
 			-- <leader><leader><leader> bdw Start of word
 			-- <leader><leader><leader> bde End of word
 			-- <leader><leader><leader> bdjk Start of line
-			{ "<leader><leader><leader>j", "<cmd>HopAnywhere<cr>", desc = "JumpToAnywhere motion" },
+			{ "<leader><leader><leader>j", "<cmd>HopAnywhere<cr>", desc = "JumpToAnywhere motion", mode = { "n", "v" } },
 
 			-- Custom stuff I have added
-			{ "<leader><leader>0", "<cmd>HopLine<cr>", desc = "Hop begining of line" },
+			{ "<leader><leader>0", "<cmd>HopLine<cr>", desc = "Hop begining of line", mode = { "n", "v" } },
 		},
 	},
 }
