33e7fafe6ac50dc5bf2176f176f91f257013d7af
Brandon Roehl <me@brandonroehl.org>
2025-12-22 14:37:13 -0600

    schedule auto commands

--- config/nvim/lua/plugins/lsp/progress/callbacks.lua
+++ config/nvim/lua/plugins/lsp/progress/callbacks.lua
@@ -1,10 +1,12 @@
 local M = {}
 
 function M:create_autocmds()
-	local group = vim.api.nvim_create_augroup("lazy-lsp-progress", { clear = true })
+	vim.schedule(function()
+		local group = vim.api.nvim_create_augroup("lazy-lsp-progress", { clear = true })
 
-	require("plugins.lsp.progress.snacks"):create_autocmd(group)
-	require("plugins.lsp.progress.ghostty"):create_autocmd(group)
+		require("plugins.lsp.progress.snacks"):create_autocmd(group)
+		require("plugins.lsp.progress.ghostty"):create_autocmd(group)
+	end)
 end
 
 return M
