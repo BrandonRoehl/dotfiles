b0f419672214bbab486d2d9fb210ed8649b6d5cd
Brandon Roehl <me@brandonroehl.org>
2025-12-23 09:23:08 -0600

    some nonsense

--- config/nvim/lua/config/utils/computer.lua
+++ config/nvim/lua/config/utils/computer.lua
@@ -1,11 +1,11 @@
 ---@class utils.computer
-local M = {
-	-- https://www.lua.org/pil/17.1.html
-	-- Memoize Functions
-	---@type string[] if the computer goes by this name
-	__host = {},
-}
-setmetatable(M.__host, { __mode = "kv" })
+local M = {}
+
+-- https://www.lua.org/pil/17.1.html
+-- Memoize Functions
+---@type string[] if the computer goes by this name
+M._host = {}
+setmetatable(M._host, { __mode = "kv" })
 
 function M.is_win()
 	return jit.os:find("Windows") or vim.fn.has("win32") == 1
@@ -19,20 +19,20 @@ end
 ---@return boolean true if the computer name matches
 function M:is_host(...)
 	-- Cache the lookup of names so future checks are quicker
-	if #self.__host == 0 then
-		self.__host = { vim.fn.hostname() }
+	if #self._host == 0 then
+		self._host = { vim.fn.hostname() }
 		-- On macOS, get the computer name instead of just the hostname
 		if self.is_mac() then
 			local obj = vim.system({ "scutil", "--get", "ComputerName" }, { text = true }):wait()
 			if obj.code == 0 then
 				local out, _ = string.gsub(obj.stdout, "\n$", "")
-				table.insert(self.__host, out)
+				table.insert(self._host, out)
 			end
 		end
 	end
 
 	return vim.tbl_contains({ ... }, function(name)
-		return vim.list_contains(self.__host, name)
+		return vim.list_contains(self._host, name)
 	end, { predicate = true })
 end
 
