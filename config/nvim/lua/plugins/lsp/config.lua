f01305bc5eee9d955c543231cef55192479f32e4
Brandon Roehl <me@brandonroehl.org>
2026-01-02 14:13:14 -0600

    words

--- config/nvim/lua/plugins/lsp/config.lua
+++ config/nvim/lua/plugins/lsp/config.lua
@@ -40,36 +40,6 @@ local function on_attach(event)
 	local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
 	bind_keys(client, event.buf)
 
-	-- The following autocommands are used to highlight references of the
-	-- word under your cursor when your cursor rests there for a little while.
-	--    See `:help CursorHold` for information about when this is executed
-	--
-	-- When you move your cursor, the highlights will be cleared (the second autocommand).
-	if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-		-- vim.cmd([[
-		-- hi! LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-		-- hi! LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-		-- hi! LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-		-- ]])
-		vim.api.nvim_create_augroup("lsp_document_highlight", {
-			clear = false,
-		})
-		vim.api.nvim_clear_autocmds({
-			buffer = event.buf,
-			group = "lsp_document_highlight",
-		})
-		vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-			group = "lsp_document_highlight",
-			buffer = event.buf,
-			callback = vim.lsp.buf.document_highlight,
-		})
-		vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-			group = "lsp_document_highlight",
-			buffer = event.buf,
-			callback = vim.lsp.buf.clear_references,
-		})
-	end
-
 	-- The following auto commands trigger the diagnostics for what you are hovering
 	vim.api.nvim_create_autocmd("CursorHold", {
 		buffer = event.buf,
