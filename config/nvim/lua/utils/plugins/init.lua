3b769c460301792471937f231dda66dd7a4de239
Brandon Roehl <me@brandonroehl.org>
2026-01-19 12:27:21 -0600

    init

--- config/nvim/lua/utils/plugins/init.lua
+++
@@ -1,7 +0,0 @@
--- Load custom helpers
-require("utils")
--- Register custom events before setup
-Utils.lazy.lazy_file()
-Utils.lazy.register_custom_event("LazyDap", "LspPreEnable", "LspPostEnable")
-
-return {}
