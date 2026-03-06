0c0def7f890ba795ca506dfa4e8547e776c75941
Brandon Roehl <me@brandonroehl.org>
2026-03-05 21:12:03 -0600

    tokyo

--- config/nvim/lua/plugins/themes/init.lua
+++ config/nvim/lua/plugins/themes/init.lua
@@ -1,8 +1,8 @@
 ---@module "lazy"
 ---@type LazyPluginSpec
 return {
-	-- import = "plugins.themes.tokyonight",
+	import = "plugins.themes.tokyonight",
 	-- import = "plugins.themes.kanagawa",
-	import = "plugins.themes.catppuccin",
+	-- import = "plugins.themes.catppuccin",
 	-- import = "plugins.themes.github",
 }
