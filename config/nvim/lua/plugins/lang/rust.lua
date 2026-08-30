7e952d8a4738872f6aa79244d5fdbfcf5cb29a64
Brandon Roehl <me@brandonroehl.org>
2026-08-29 23:25:45 -0500

    loop break

--- config/nvim/lua/plugins/lang/rust.lua
+++ config/nvim/lua/plugins/lang/rust.lua
@@ -6,25 +6,33 @@ local function expand_macro()
 	local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "rust_analyzer" })
 
 	for _, client in ipairs(clients) do
-		local params = vim.lsp.util.make_position_params(win, client.offset_encoding)
-		if
-			client:supports_method("rust-analyzer/expandMacro", bufnr)
-			and client:request("rust-analyzer/expandMacro", params, function(err, result)
-				if err then
-					vim.notify("Expand macro failed: " .. (err.message or vim.inspect(err)), vim.log.levels.ERROR)
-					return
-				end
-				if not result or not result.expansion or result.expansion == "" then
-					vim.notify("No macro under cursor", vim.log.levels.INFO)
-					return
-				end
-				vim.lsp.util.open_floating_preview(vim.split(result.expansion, "\n", { trimempty = true }), "rust", {
-					title = " " .. (result.name or "Macro expansion") .. " ",
-					border = "rounded",
-				})
-			end, bufnr)
-		then
-			return
+		if client:supports_method("rust-analyzer/expandMacro", bufnr) then
+			local status, _ = client:request(
+				"rust-analyzer/expandMacro",
+				vim.lsp.util.make_position_params(win, client.offset_encoding),
+				function(err, result)
+					if err then
+						vim.notify("Expand macro failed: " .. (err.message or vim.inspect(err)), vim.log.levels.ERROR)
+						return
+					end
+					if not result or not result.expansion or result.expansion == "" then
+						vim.notify("No macro under cursor", vim.log.levels.INFO)
+						return
+					end
+					vim.lsp.util.open_floating_preview(
+						vim.split(result.expansion, "\n", { trimempty = true }),
+						"rust",
+						{
+							title = " " .. (result.name or "Macro expansion") .. " ",
+							border = "rounded",
+						}
+					)
+				end,
+				bufnr
+			)
+			if status then
+				return
+			end
 		end
 	end
 	vim.notify("rust-analyzer is not attached to this buffer", vim.log.levels.WARN)
