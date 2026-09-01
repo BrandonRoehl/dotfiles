2fc5342914c8c41a5d36f5cc2a845caab406bd1f
Brandon Roehl <brandon.roehl@jamf.com>
2026-09-01 13:58:53 -0500

    neovide

--- config/nvim/lua/config/program/neovide.lua
+++ config/nvim/lua/config/program/neovide.lua
@@ -2,3 +2,7 @@
 -- https://neovide.dev/configuration.html
 
 vim.g.have_nerd_font = true
+
+vim.g.neovide_cursor_trail_size = 0.8
+vim.g.neovide_cursor_animation_length = 0.10
+vim.g.neovide_cursor_short_animation_length = 0.02
