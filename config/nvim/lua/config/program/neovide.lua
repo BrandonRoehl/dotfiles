d400bab26e457bdaf52c17ad6fc5fed198fa20df
Brandon Roehl <brandon.roehl@jamf.com>
2026-09-02 15:05:46 -0500

    hide mouse

--- config/nvim/lua/config/program/neovide.lua
+++ config/nvim/lua/config/program/neovide.lua
@@ -6,3 +6,4 @@ vim.g.have_nerd_font = true
 vim.g.neovide_cursor_trail_size = 0.8
 vim.g.neovide_cursor_animation_length = 0.10
 vim.g.neovide_cursor_short_animation_length = 0.02
+vim.g.neovide_hide_mouse_when_typing = true
